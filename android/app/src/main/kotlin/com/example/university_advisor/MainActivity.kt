508e8ca61b3ad08ce4e8c9da3f278aee56d9a9bd
Deividas Balandis <deividas.balandis@gmail.com>
2019-12-28 19:48:57 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/university_advisor/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.university_advisor
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: MyApp())
+  );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Image.asset("assets/logo_midsize.png"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
